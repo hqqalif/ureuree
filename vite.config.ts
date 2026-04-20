cfa2441fdcecbeace2fb2679ab670275f78d6597
foreva <hakikialifhabibullah@gmail.com>
2026-04-20 13:03:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: './',
+  base: '/ureuree/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
